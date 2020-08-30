94637086c29208c584ffa37163ec3c5a4e066f06
Ayarza Manwaring <ayarza.manwaring@gmail.com>
2020-08-30 01:31:05 -0400

    backend changes didn't get saved in the last commit

--- server/api/index.js
+++ server/api/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* Test api routing */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.send('Welcome to your express server! Start creating and editing your api routes in the server/api folder.');
 });
+
+router.post('/', (req, res, next) => {
+  res.send(`This message is sent from the post route: ${req.body.post}`)
+})
 
 module.exports = router;
